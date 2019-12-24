ae4933f3c10ac9fea048f6d24d8de9fefd8bd9b1
Tony Perez <tonper19@gmail.com>
2019-12-24 10:41:49 +0100

    VS Code examples on how to edit and lint

---
+++ vs_code.py
@@ -0,0 +1,9 @@
+from os import path
+
+print(f'The path.curdir is: {path.curdir}')
+print(f'The path.defpath is: {path.defpath}')
+print(f'The path.devnull is: {path.devnull}')
+print(f'The path.supports_unicode_filenames is: {path.supports_unicode_filenames}')
+
+
+
