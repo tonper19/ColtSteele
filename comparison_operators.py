599611f1d4d3c436ede302ee38d4f9575c334461
Tony Perez <tonper19@gmail.com>
2019-12-24 10:40:08 +0100

    pep8

--- comparison_operators.py
+++ comparison_operators.py
@@ -9,12 +9,12 @@ age = 66
 pension = 50000
 
 if age <= 6:
-  print('you must go to kindergarden')
+    print('you must go to kindergarden')
 elif age <= 17:
-  print('you must go to school')
+    print('you must go to school')
 elif age < 65:
-  print('you must go to work')
+    print('you must go to work')
 elif age > 65 or pension > 999999:
-  print('you can retire')
+    print('you can retire')
 else:
-  print('you cannot retire because you dont have enough money in your pension')
+    print('you cannot retire because you dont have enough money in your pension')
