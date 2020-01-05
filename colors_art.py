bd0b152928d3b208c9be91d8947e2107e30f3885
Tony Perez <tonper19@gmail.com>
2020-01-05 19:14:18 +0100

    refactoring

--- colors_art.py
+++ colors_art.py
@@ -1,15 +1,20 @@
-import pyfiglet
+from pyfiglet import figlet_format
 from termcolor import colored
-
-msg = input('Please enter a text: ')
-color = input('Please enter a color: ')
 
 valid_colors = ('red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white')
 
-if color not in valid_colors:
-    msg = f'The color {color} is not valid, using green instead...'
-    color = 'green'
 
-ascii_art = pyfiglet.figlet_format(msg)
-colored_ascii = colored(ascii_art, color=color)
-print(colored_ascii)
+def print_art(msg, color):
+    if color not in valid_colors:
+        msg = f'The color {color} is not valid, using green instead...'
+        color = 'green'
+
+    ascii_art = figlet_format(msg)
+    colored_ascii = colored(ascii_art, color=color)
+    print(colored_ascii)
+
+
+if __name__ == "__main__":
+    msg = input('Please enter a text: ')
+    color = input('Please enter a color: ')
+    print_art(msg, color)
