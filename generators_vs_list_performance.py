972df55c76185fb29e847518bdb86422a067e102
Tony Perez <tonper19@gmail.com>
2020-01-09 21:08:20 +0100

    performance comparison between generator and list comprehension

---
+++ generators_vs_list_performance.py
@@ -0,0 +1,29 @@
+"""
+generators_vs_list_performance.py
+
+compare the times to sum a sequence of numbers using a generator comprehension
+and a list comprehension
+
+Output:
+*** generator comprehension calculating the sum of the first 9999999999
+49999999985000000001
+*** list comprehension calculating the sum of the first 9999999999
+49999999985000000001
+the generator comprehension took 595.0286509990692
+the list comprehension took 599.2130289077759
+"""
+
+import time
+
+generator_start_time = time.time()
+print("*** generator comprehension calculating the sum of the first 9999999999")
+print(sum(n for n in range(9999999999)))
+generator_time = time.time() - generator_start_time
+
+list_start_time = time.time()
+print("*** list comprehension calculating the sum of the first 9999999999")
+print(sum(n for n in range(9999999999)))
+list_time = time.time() - list_start_time
+
+print(f"the generator comprehension took {generator_time}")
+print(f"the list comprehension took {list_time}")
