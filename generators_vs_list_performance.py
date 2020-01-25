e784f4f501b2fbc584245899619c7d23fce364c4
Tony Perez <tonper19@gmail.com>
2020-01-25 17:49:55 +0100

    changed the performance counter

--- generators_vs_list_performance.py
+++ generators_vs_list_performance.py
@@ -5,9 +5,9 @@ compare the times to sum a sequence of numbers using a generator comprehension
 and a list comprehension
 
 Output:
-*** generator comprehension calculating the sum of the first 9999999999
+*** generator comprehension calculating the sum of the first 9999999
 49999999985000000001
-*** list comprehension calculating the sum of the first 9999999999
+*** list comprehension calculating the sum of the first 9999999
 49999999985000000001
 the generator comprehension took 595.0286509990692
 the list comprehension took 599.2130289077759
@@ -15,15 +15,16 @@ the list comprehension took 599.2130289077759
 
 import time
 
-generator_start_time = time.time()
-print("*** generator comprehension calculating the sum of the first 9999999999")
-print(sum(n for n in range(9999999999)))
-generator_time = time.time() - generator_start_time
+generator_start_time = time.perf_counter()
+print("*** generator comprehension calculating the sum of the first 9999999")
+print(sum(n for n in range(9999999)))
+generator_time = time.perf_counter() - generator_start_time
 
-list_start_time = time.time()
-print("*** list comprehension calculating the sum of the first 9999999999")
-print(sum(n for n in range(9999999999)))
-list_time = time.time() - list_start_time
+list_start_time = time.perf_counter()
+print("*** list comprehension calculating the sum of the first 9999999")
+print(sum(n for n in range(9999999)))
+list_time = time.perf_counter() - list_start_time
 
 print(f"the generator comprehension took {generator_time}")
 print(f"the list comprehension took {list_time}")
+print(f"Clock resolution: {time.get_clock_info('perf_counter').resolution}")
