6f0211c879800d9891d5f993a954fd907485745f
Tony Perez <tonper19@gmail.com>
2020-01-27 21:58:50 +0100

    from Python fundamentals' Modularity chapter

---
+++ python_modularity.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Python modularity example
+
+Retrieve and print words from a URL.
+
+Usage:
+    python3 python_modularity.py <URL>
+
+test url: "http://sixty-north.com/c/t.txt"
+AUTHOR
+    Tony Perez
+
+DATE
+    27/01/2020
+
+"""
+import sys
+from urllib.request import urlopen
+
+def fetch_words(url):
+    """
+    Fetch a list of words from a URL
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    
+    Returns:
+        A list of strings containing the words from
+        the document.
+    """
+    with urlopen(url) as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode("utf-8").split()
+            for word in line_words:
+                story_words.append(word)
+    return story_words
+
+
+def print_items(items):
+    """Print items one per line.
+    
+    Args:
+        An iterable series of printable items.
+    """
+    for item in items:
+        print(item)
+
+
+def main(url):
+    """Print each word from a text document from a URL.
+    
+    Args:
+        url: The URL of a UTF-8 text document.
+    """
+    words = fetch_words(url)
+    print_items(words)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])  # first command line argument in stand-alone
