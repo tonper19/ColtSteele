d97f61cfad2b96a4f461339b9df506fb4aa7f16c
Tony Perez <tonper19@gmail.com>
2020-02-08 00:27:53 +0100

    Simple unit test example + pytest

---
+++ costurero/pytest/test_math.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def inc(x):
+    return x + 1
+
+def test_add():
+    assert add(1, 2) == 3
+def test_subtract():
+    assert subtract(2, 1) == 1
+def test_inc():
+    assert inc(1) == 2
