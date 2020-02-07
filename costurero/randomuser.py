39d5855ed874e03edcfb080396c32faa2308839a
Tony Perez <tonper19@gmail.com>
2020-02-08 00:09:03 +0100

    autoDocString

--- costurero/randomuser.py
+++ costurero/randomuser.py
@@ -6,8 +6,22 @@ Check Arepl extension in VSCode.
 """
 import requests
 
-response = requests.get("https://randomuser.me/api/?results=10")
-data = response.json()
+def rand(url="https://randomuser.me/api/?results=10"):
+    """[summary]
+    
+    Keyword Arguments:
+        url {str} -- [description] (default: {"https://randomuser.me/api/?results=10"})
+    
+    Returns:
+        [type] -- [description]
+    """
+    response = requests.get(url)
+    data = response.json()
 
-for user in data["results"]:
-    print(user["name"]["last"])
+    for user in data["results"]:
+        print(user["name"]["last"])
+    
+    return data
+
+if __name__ == "__main__":
+    print(rand())
