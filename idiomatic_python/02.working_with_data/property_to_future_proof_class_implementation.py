afbf5b2b69d1b6018bf5431bc09afe5062cb5939
Tony Perez <tonper19@gmail.com>
2020-02-12 17:41:47 +0100

    working with data

--- idiomatic_python/02.working_with_data/property_to_future_proof_class_implementation.py
+++ idiomatic_python/02.working_with_data/property_to_future_proof_class_implementation.py
@@ -9,9 +9,17 @@ class Product():
     def price(self):
         # now if we need to change how price is calculated, we can do it 
         # here (or in the "setter" and __init__)
-        return self._price * TAX_RATE
+        return self._price + self._price * Product.TAX_RATE
 
     @price.setter
     def price(self, value):
         # The "setter" function must have the same name as the property
         self._price = value
+
+
+def main():
+    p = Product("Macbook", 4200)
+    print(f"The price of {p.name} is {p.price}")
+
+if __name__ == "__main__":
+    main()
